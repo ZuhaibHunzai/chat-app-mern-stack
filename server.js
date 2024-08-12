0499ca214f21278ff4280233176fba7dd78ea132
ZuhaibHunzai <zuhaibalamhunzai188@gmail.com>
2024-08-12 23:46:10 +0500

    db connected

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config({});
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+const app = express();
+const { connectDb } = require("./configs/dbConfig");
+connectDb();
+
+// json request => parse -> request.body attach
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+
+// app.use(cors(corsOptions));
+// app.use("/api", require("./routes"));
+
+// app.use("*", (req, res) => {
+//   res.status(404).json({ message: "path not found" });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: http://localhost:${PORT}`);
+});
